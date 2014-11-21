1b46923f6a19a1f3c73c6cfc9dc6cf5d9b03a449
Cory Diamand <cory@postpourri.com>
2014-11-21 14:40:49 -0500

    move award properties into Award struct and small refactor of update_quality_method

--- award.rb
+++ award.rb
@@ -1,1 +1,38 @@
-Award = Struct.new(:name, :expires_in, :quality)
+Award = Struct.new(:name, :expires_in, :quality) do
+
+	QUALITY_MAX = 50
+	QUALITY_MIN = 0
+
+	def below_quality_max?
+		quality < QUALITY_MAX
+	end
+
+	def above_quality_min?
+		quality > QUALITY_MIN
+	end
+
+	def blue_distinction_plus?
+		name == 'Blue Distinction Plus'
+	end
+
+	def blue_first?
+		name == 'Blue First'
+	end
+
+	def blue_compare?
+		name == 'Blue Compare'
+	end
+
+	def decrease_expiration
+		self.expires_in -= 1
+	end
+
+	def increase_quality
+		self.quality += 1
+	end
+
+	def decrease_quality
+		self.quality -= 1
+	end
+
+end
--- update_quality.rb
+++ update_quality.rb
@@ -2,46 +2,46 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
+    if !award.blue_first? && !award.blue_compare?
+      if award.above_quality_min?
+        if !award.blue_distinction_plus?
+          award.decrease_quality
         end
       end
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
+      if award.below_quality_max?
+        award.increase_quality
+        if award.blue_compare?
           if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
+            if award.below_quality_max?
+              award.increase_quality
             end
           end
           if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
+            if award.below_quality_max?
+              award.increase_quality
             end
           end
         end
       end
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
+    if !award.blue_distinction_plus?
+      award.decrease_expiration
     end
     if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
+      if !award.blue_first?
+        if !award.blue_compare?
+          if award.above_quality_min?
+            if !award.blue_distinction_plus?
+              award.decrease_quality
             end
           end
         else
           award.quality = award.quality - award.quality
         end
       else
-        if award.quality < 50
-          award.quality += 1
+        if award.below_quality_max?
+          award.increase_quality
         end
       end
     end
